7e988700c938acd3ee819de18e8ea21c059de51d
Alexxanderrx <133218226+Alexxanderrx@users.noreply.github.com>
2023-05-23 14:46:29 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,18 +7,18 @@ const dia_actual = fecha_actual.getDate();
 let mes_actual = fecha_actual.getMonth() + 1;
 let anho_actual = fecha_actual.getFullYear();
 
-// const date_error = document.getElementsByClassName("date_error");
-
 const btn = document.getElementById("submit");
 
 const span_day = document.getElementById("span_day");
 const span_month = document.getElementById("span_mon");
 const span_year = document.getElementById("span_yea");
 
+
 btn.addEventListener("click", (e) => {
     e.preventDefault();
     activar();
 });
+
 
 function activar() {
     find_dia();
@@ -35,18 +35,19 @@ function activar() {
 const mes_w_30 = ["2", "4", "6", "9", "11"]
 const mes_w_31 = ["1", "3", "5", "7", "8", "10", "12"]
 
+
 function find_dia() {
     let mostrar_dia = dia_actual - dia.value;
 
     if (mostrar_dia < 0 && mes_w_30.includes(mes.value)) {
         mostrar_dia = 30 + mostrar_dia;
         span_day.innerHTML = mostrar_dia;
-        mes_actual--
+        // mes_actual--
     } else if (mostrar_dia < 0 && mes_w_31.includes(mes.value)) {
         mostrar_dia = 31 + mostrar_dia;
         span_day.innerHTML = mostrar_dia;
 
-        mes_actual--
+        // mes_actual--
     } else {
         span_day.innerHTML = mostrar_dia;
     };
@@ -55,14 +56,27 @@ function find_dia() {
 function find_mes() {
     let mostrar_mes = mes_actual - mes.value;
 
-    if (mostrar_mes < 0) {
-        mostrar_mes = 12 + mostrar_mes;
-        span_month.innerHTML = mostrar_mes;
-
-        // anho_actual--
+    if (dia_actual < dia.value) {
+        if (mostrar_mes < 0) {
+            mostrar_mes = 12 + mostrar_mes - 1;
+            span_month.innerHTML = mostrar_mes;
+            console.log ("b");
+            // anho_actual--
+        }else {
+            span_month.innerHTML = mostrar_mes;
+            console.log ("c");
+        };
     } else {
-        span_month.innerHTML = mostrar_mes;
-    };
+        if (mostrar_mes < 0) {
+            mostrar_mes = 12 + mostrar_mes;
+            span_month.innerHTML = mostrar_mes;
+            console.log ("baa");
+            // anho_actual--
+        } else {
+            span_month.innerHTML = mostrar_mes;
+            console.log ("caa");
+        };
+    }
 };
 
 function find_anhos() {
@@ -75,13 +89,20 @@ function find_anhos() {
 };
 
 function val_dia() {
+
     dia.value = parseInt (dia.value);
+
     if (dia.value > 31 || dia.value < 0) {
         document.getElementById("lab_day").style.color = 'rgb(193, 49, 69)';
         dia.style.border = '1px solid rgb(193, 49, 69)';
         document.getElementById("day_error").style.color = 'rgb(193, 49, 69)';
         error();
-    } else {
+    }else if (dia_actual < dia.value && (mes_actual - mes.value) == 0) {
+        document.getElementById("lab_day").style.color = 'rgb(193, 49, 69)';
+        dia.style.border = '1px solid rgb(193, 49, 69)';
+        document.getElementById("day_error").style.color = 'rgb(193, 49, 69)';
+        error();
+    }  else {
         document.getElementById("lab_day").style.color = 'rgb(163, 163, 163)';
         dia.style.border = '1px solid rgb(0, 0, 0)';
         document.getElementById("day_error").style.color = 'transparent';
@@ -90,8 +111,9 @@ function val_dia() {
 }
 
 function val_mon() {
-//   Deve colocarse esto ↓ o sino los valores con 0 al principio no lo reconocera bien
+
     mes.value = parseInt (mes.value);
+
     if (mes.value > 12 || mes.value < 0) {
         document.getElementById("lab_month").style.color = 'rgb(193, 49, 69)';
         mes.style.border = '1px solid rgb(193, 49, 69)';
@@ -105,7 +127,9 @@ function val_mon() {
 }
 
 function val_yea() {
+
     anho.value = parseInt(anho.value);
+
     if (anho.value > anho_actual) {
         document.getElementById("lab_year").style.color = 'rgb(193, 49, 69)';
         anho.style.border = '1px solid rgb(193, 49, 69)';
